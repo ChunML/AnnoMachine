d36895db145d6bbb46a938d0e7385bb415d34233
Trung Tran <trungtran.chunml@gmail.com>
2019-10-10 23:31:45 +0900

    Add docker files for production

---
+++ services/backend/entrypoint_prod.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Waitin for postgres to start..."
+
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+python manage.py recreate_db
+
+gunicorn -b 0.0.0.0:5000 manage:app
