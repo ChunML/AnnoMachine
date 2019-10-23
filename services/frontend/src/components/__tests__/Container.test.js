a817f123aa1c4b340b5507f268c8bb404176043f
Trung Tran <trungtran.chunml@gmail.com>
2019-10-23 17:21:37 +0900

    Update test for Container.js

--- services/frontend/src/components/__tests__/Container.test.js
+++ services/frontend/src/components/__tests__/Container.test.js
@@ -1,21 +1,124 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { MemoryRouter as Router } from 'react-router-dom';
+import renderer from 'react-test-renderer';
 import Container from '../Container';
+import ImageTabNavBar from '../ImageTabNavBar';
 import '../../setupTests';
 
-it('Container renders properly', () => {
-  const wrapper = shallow(
-    <Container
-      images={[{}]}
-      onButtonClick={() => {}}
-      onTabChange={() => {}}
-      onDeleteImage={() => {}}
-      isAuthenticated={false}
-      isLoading={false}
-      currentUser=''
-      selectedTab=''
-    />
-  );
-  const container = wrapper.find('.container');
-  expect(container).toBeDefined();
+const images = [{
+  id: 1,
+  name: 'test.jpg',
+  width: 100,
+  height: 100,
+  user: {
+    username: 'testUser'
+  },
+  uploaded_at: '2000/01/01 01:02',
+  boxes: [{
+    id: 1,
+    x_min: 10,
+    y_min: 10,
+    x_max: 20,
+    y_max: 20,
+    label: 'dog'
+  }]
+}];
+
+describe('Image dashboard page', () => {
+  it('Container renders properly', () => {
+    const wrapper = mount(
+      <Router initialEntries={['/images']}>
+        <Container
+          images={ images }
+          onButtonClick={ jest.fn() }
+          onTabChange={ jest.fn() }
+          onDeleteImage={ jest.fn() }
+          isAuthenticated={ false }
+          isLoading={ false }
+          currentUser='testUser'
+          selectedTab='all'
+        />
+      </Router>
+    );
+    expect(wrapper.find(ImageTabNavBar)).toHaveLength(1);
+    expect(wrapper.find('CardList')).toHaveLength(1);
+    expect(wrapper.find('AddImageForm')).toHaveLength(0);
+  });
+  
+  it('Container renders properly with authenticated user', () => {
+    const wrapper = mount(
+      <Router initialEntries={['/images']}>
+        <Container
+          images={ images }
+          onButtonClick={ jest.fn() }
+          onTabChange={ jest.fn() }
+          onDeleteImage={ jest.fn() }
+          isAuthenticated={ true }
+          isLoading={ false }
+          currentUser='testUser'
+          selectedTab='all'
+        />
+      </Router>
+    );
+    expect(wrapper.find(ImageTabNavBar)).toHaveLength(1);
+    expect(wrapper.find('CardList')).toHaveLength(1);
+    expect(wrapper.find('AddImageForm')).toHaveLength(1);
+  });
+  
+  it('Container renders a snapshot properly', () => {
+    const tree = renderer.create(
+      <Router initialEntries={['/images']}>
+        <Container
+          images={ images }
+          onButtonClick={ jest.fn() }
+          onTabChange={ jest.fn() }
+          onDeleteImage={ jest.fn() }
+          isAuthenticated={ false }
+          isLoading={ false }
+          currentUser='testUser'
+          selectedTab='all'
+        />
+      </Router>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Image detail page', () => {
+  it('Container renders properly', () => {
+    const wrapper = mount(
+      <Router initialEntries={['/images/test']}>
+        <Container
+          images={ images }
+          onButtonClick={ jest.fn() }
+          onTabChange={ jest.fn() }
+          onDeleteImage={ jest.fn() }
+          isAuthenticated={ false }
+          isLoading={ false }
+          currentUser='testUser'
+          selectedTab='all'
+        />
+      </Router>
+    );
+    expect(wrapper.find('ImageDetail')).toHaveLength(1);
+  });
+
+  it('Container renders a snapshot properly', () => {
+    const tree = renderer.create(
+      <Router initialEntries={['/images/test']}>
+        <Container
+          images={ images }
+          onButtonClick={ jest.fn() }
+          onTabChange={ jest.fn() }
+          onDeleteImage={ jest.fn() }
+          isAuthenticated={ false }
+          isLoading={ false }
+          currentUser='testUser'
+          selectedTab='all'
+        />
+      </Router>
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
