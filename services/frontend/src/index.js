1eb03dbf26bb02d03f8c067802ad96ee23b310a7
Trung Tran <trungtran.chunml@gmail.com>
2019-11-01 16:30:44 +0900

    Styled Navbar and Footer

--- services/frontend/src/index.js
+++ services/frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <Router>
